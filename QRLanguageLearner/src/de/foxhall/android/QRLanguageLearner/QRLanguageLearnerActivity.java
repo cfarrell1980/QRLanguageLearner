8c1bc016507f4d7c59634e9c31907b6a0cbe50d8
Ciaran Farrell <cfarrell1980@gmail.com>
2012-01-20 21:31:07 +0100

    First commit

---
+++ QRLanguageLearner/src/de/foxhall/android/QRLanguageLearner/QRLanguageLearnerActivity.java
@@ -0,0 +1,77 @@
+package de.foxhall.android.QRLanguageLearner;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class QRLanguageLearnerActivity extends Activity implements TextToSpeech.OnInitListener{
+    private TextToSpeech mTts;
+    private static final int MY_DATA_CHECK_CODE = 2109;
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
+    }
+
+    public void SayIt(String whatToSay){
+    	mTts.speak(whatToSay,TextToSpeech.QUEUE_FLUSH,null);
+    }
+    
+    public void onInit(int i){
+    	
+    }
+
+    public void ScanHandler(View v){
+    	IntentIntegrator.initiateScan(this);
+    }
+    
+
+    // Callback    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	
+    	switch(requestCode) {
+    	  case MY_DATA_CHECK_CODE: {
+    		  if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS)
+              {
+                  // success, create the TTS instance
+                  mTts = new TextToSpeech(this, this);
+              }
+              else
+              {
+                  // missing data, install it
+                  Intent installIntent = new Intent();
+                  installIntent.setAction(
+                          TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                  startActivity(installIntent);
+              } 
+    	  }
+    	  case IntentIntegrator.REQUEST_CODE: {
+    		// Log looks like W/TestTextActivity.requestCode(11382): 195543262
+    		Log.w("TestTextActivity.requestCode",String.valueOf(requestCode));
+    		if (resultCode != RESULT_CANCELED) {
+              IntentResult scanResult = 
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+    		  if (scanResult != null) {
+    	        String upc = scanResult.getContents();    	 
+                SayIt(upc);
+    	        Toast.makeText(this, upc, 3).show();
+    	       
+    	      }
+    		  else {
+    			  Toast.makeText(this, "Barcode scan failed!", 3).show();
+    		  }
+    	    }
+    	    break;
+    	  }
+    	  
+       }
+      }
+
+}
