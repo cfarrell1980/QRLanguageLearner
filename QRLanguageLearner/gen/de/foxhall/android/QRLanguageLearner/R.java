bf4a373a444f0f484175d5cdbf720c82d0979021
Ciaran Farrell <cfarrell1980@gmail.com>
2012-01-21 13:50:36 +0100

    rename 1.mp3 and 2.mp3 as android uses the filename (1 and 2) as variables - this obviously goes horribly wrong

--- QRLanguageLearner/gen/de/foxhall/android/QRLanguageLearner/R.java
+++ QRLanguageLearner/gen/de/foxhall/android/QRLanguageLearner/R.java
@@ -14,14 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
+        public static final int button1=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int one=0x7f040000;
+        public static final int two=0x7f040001;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int bcScan=0x7f040002;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int bcScan=0x7f050002;
+        public static final int hello=0x7f050000;
     }
 }
